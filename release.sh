32b1a1e09a1091146942eb8d1c5f8ae11c47c86f
Phie <phie@phie.ovh>
2020-05-08 11:11:18 +0200

    update release script

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git pull --rebase
+git checkout master
+git pull --rebase origin master
 cd app/CarnetAndroid
 git pull --rebase origin stable
 cd ../Sync
@@ -7,7 +8,7 @@ git pull --rebase origin master
 cd ../../
 git add app/CarnetAndroid app/Sync
 git commit -m "sync"
-git push
+git push origin master
 
 git tag v$(grep -o "versionName\s\+.*" app/CarnetAndroid/app/build.gradle | tail -1 | awk '{ print $2 }' | tr -d \''"\')
 git push origin v$(grep -o "versionName\s\+.*" app/CarnetAndroid/app/build.gradle | tail -1 | awk '{ print $2 }' | tr -d \''"\')
